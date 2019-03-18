d8a9819f6cbc4269fe118edd65fd7583ce249d38
easysky <cyuffei@foxmail.com>
2019-03-18 14:35:51 +0800

    Add files via upload

--- s.js
+++ s.js
@@ -1,7 +1,7 @@
 var ua=/Android|webOS|iPhone|iPod|BlackBerry/i.test(navigator.userAgent)?1:0,ss=[[(ua?"m":"www")+".baidu.com","/s?word=","百度"],["cn.bing.com","/search?q=","必应"],[(ua?"m":"www")+".so.com","/s?q=","360"],[(ua?"wap":"www")+".sogou.com","/web"+(ua?"/searchlist.jsp?keyword=":"?query="),"搜狗"],[(ua?"m.":"")+"mijisou.com","/?q=","秘迹"],["yandex.com","/search/?text=","Yandex"],["vip.kuaimen.bid","/search?&q=","谷歌"]],sk=window.location.search,si=0,yid;
 $(function(){
 	var str="";
-	$("#main h2").text($("title").text());
+	//$("#main h2").text($("title").text());
 	for (var i=0;i<ss.length;i++){
 		str+="<span>"+ss[i][2]+"</span>";
 	}
@@ -17,6 +17,9 @@ $(function(){
 		}else{
 			sk=decodeURI(sk);
 		}
+		$(".loading").show();
+	}else{
+		$("#main h2").text($("title").text());
 	}
 	$("#box span").eq(si).css("background-color","#080");
 	$("input").val(sk);
